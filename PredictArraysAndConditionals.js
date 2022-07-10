f4ac8295642bed903482f916966ce15fb2699c7f
Brock Ericksen <brockericksen@gmail.com>
2022-07-10 16:23:23 -0400

    This is all of my pre-bootcamp work I did for Coding Dojo

---
+++ PredictArraysAndConditionals.js
@@ -0,0 +1,25 @@
+function displayContactInfo() {
+    var auntContactInfo = ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+
+// Paula Smith 1234 main street st. louis mo 12345
+
+
+function displayGrocerylist() {
+    var produce = ["apples", "oranges"];
+    var frozen = ["broccoli", "ice cream"];
+    frozen.push("hashbrowns");
+    console.log(frozen);
+}
+
+// hashbrowns broccoli ice cream
+
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+
+// zero beetlejuice E.T. toy story
+
